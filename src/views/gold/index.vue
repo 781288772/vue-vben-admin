7dac53a578687686b251669045c83fd5f77fc0ca
chy <781288772@qq.com>
2022-07-22 11:45:10 +0800

    宝贝列表渲染

--- src/views/gold/index.vue
+++ src/views/gold/index.vue
@@ -1,19 +1,21 @@
 <template>
   <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-    <template #footer>
+    <!-- <template #footer>
       <div>
         <b>ant design vue</b>
         footer part
       </div>
-    </template>
+    </template> -->
     <template #renderItem="{ item }">
-      <a-list-item key="item.title">
-        <template #actions>
+
+    {{item.name}}
+      <a-list-item key="item.name">
+        <!-- <template #actions>
           <span v-for="{ type, text } in actions" :key="type">
             <component :is="type" style="margin-right: 8px" />
             {{ text }}
           </span>
-        </template>
+        </template> -->
         <template #extra>
           <img
             width="272"
@@ -23,11 +25,11 @@
         </template>
         <a-list-item-meta :description="item.description">
           <template #title>
-            <a :href="item.href">{{ item.title }}</a>
+            {{ item.name }}
           </template>
-          <template #avatar><a-avatar :src="item.avatar" /></template>
+          <!-- <template #avatar><a-avatar :src="item.avatar" /></template> -->
         </a-list-item-meta>
-        {{ item.content }}
+        {{ item.price }}
       </a-list-item>
     </template>
   </a-list>
@@ -35,29 +37,54 @@
 
 <script lang="ts" setup>
 import { defHttp } from '/@/utils/http/axios';
+import {reactive,onMounted} from "vue";
 
-defHttp.get({
+let listData: Record<string, string>[] = reactive([]);
+
+onMounted(()=>{
+  getList();
+
+ console.log(listData)
+})
+
+ const getList =  async ()=>{
+  try{
+      const res =  await defHttp.get({
   url: '/gold/list',
   params: {
     current: 1,
     size: 10,
-  },
-});
+  } 
+},
+  {
+      errorMessageMode: 'modal',
+  })
+  
+  listData .push(...res.list);
+  console.log('listData',listData)
+
+  }catch(err){
+    console.log(err)
+  }
+
+ 
+
+ }
 
 
-const listData: Record<string, string>[] = [];
 
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://www.antdv.com/',
-    title: `ant design vue part ${i}`,
-    avatar: 'https://joeschmoe.io/api/v1/random',
-    description:
-      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
+
+// for (let i = 0; i < 23; i++) {
+//   listData.push({
+//     href: 'https://www.antdv.com/',
+//     title: `ant design vue part ${i}`,
+//     avatar: 'https://joeschmoe.io/api/v1/random',
+//     description:
+//       'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+//     content:
+//       'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+//   });
+// }
    const pagination = {
       onChange: (page: number) => {
         console.log(page);
